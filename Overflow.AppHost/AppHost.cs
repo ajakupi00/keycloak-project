f604ea140447a55693bd08f6af713799afaf226f
Arjan Jakupi <arjan.jakupi007@gmail.com>
2025-12-01 15:21:39 +0100

    search service created using typesense

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -7,6 +7,17 @@ var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data")
     .WithPgAdmin();
 
+var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+
+// This is the custom configuration we must do because there is no Host NuGet package for Typesense
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey , "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithHttpEndpoint(8108, 8108, "typesense");
+
+// This is what is going to be passed down - with reference (check below passing down of keycloak for questionSvc)
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
 var questionDb = postgres.AddDatabase("questionDb");
 
 var questionSvc = builder.AddProject<Projects.QuestionService>("question-svc")
@@ -14,5 +25,10 @@ var questionSvc = builder.AddProject<Projects.QuestionService>("question-svc")
     .WithReference(questionDb)
     .WaitFor(keycloak) // When up-running this line of code waits for Keycloak to be up-and-running before us.
     .WaitFor(questionDb);
+
+var searchSvc = builder.AddProject<Projects.SearchService>("search-svc")
+    .WithEnvironment("typesense-api-key", typesenseApiKey)
+    .WithReference(typesenseContainer)
+    .WaitFor(typesense);
 
 builder.Build().Run();
