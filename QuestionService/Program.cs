0c4bef729e5299140c0f32ab2f97ddb95af61cdb
Arjan Jakupi <arjan.jakupi007@gmail.com>
2025-12-01 13:36:22 +0100

    question service with its controllers and entities

---
+++ QuestionService/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults(); // Setups health checks and telemetry stuff
+
+builder.Services.AddAuthentication()
+    .AddKeycloakJwtBearer(serviceName: "keycloak", realm: "overflow", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "overflow";
+    });
+
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+
+app.MapDefaultEndpoints();
+
+// Get hold of DbContext and apply non-applied Migrations
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync(); // Create and apply migrations
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred seeding the DB.");
+}
+
+app.Run();
