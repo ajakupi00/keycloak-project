24466f63472a11e0d46fd4720ad7a7a204e4fd42
Arjan Jakupi <arjan.jakupi007@gmail.com>
2025-12-05 15:49:24 +0100

    yarp configuration for local production
    I have added hosts inside /etc/hosts for 127.0.0.1 proxy to id.overflow.local api.overflow.local, so I can test production locally

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -10,7 +10,8 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithRealmImport("../infra/realms") // Import dev realm
     .WithEnvironment("KC_HTTP_ENABLED", "true")
     .WithEnvironment("KC_HOSTNAME_STRICT", "false")
-    .WithEndpoint(6001, 8080, "keycloak", isExternal: true);
+    .WithEnvironment("VIRTUAL_HOST", "id.overflow.local")
+    .WithEnvironment("VIRTUAL_PORT", "8080");
 
 var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data")
@@ -60,6 +61,16 @@ var yarp = builder.AddYarp("gateway")
         yarpBuilder.AddRoute("/search/{**catch-all}", searchSvc);
     })
     .WithEnvironment("ASPNETCORE_URLS", "http://*:8001")
-    .WithEndpoint(port: 8001, targetPort: 8001, scheme: "http", name: "gateway", isExternal: true);
+    .WithEndpoint(port: 8001, targetPort: 8001, scheme: "http", name: "gateway", isExternal: true)
+    .WithEnvironment("VIRTUAL_HOST", "api.overflow.local")
+    .WithEnvironment("VIRTUAL_PORT", "8001");
+
+// NGINX configuration for production only
+if (!builder.Environment.IsDevelopment())
+{
+    builder.AddContainer("nginx-proxy", "nginxproxy/nginx-proxy", "1.8")
+        .WithEndpoint(80, 80, "nginx", isExternal: true)
+        .WithBindMount("/var/run/docker.sock", "/tmp/docker.sock", true);
+}
 
 builder.Build().Run();
