96552ada65517dc769fc530145c9368ecd937813
Arjan Jakupi <arjan.jakupi007@gmail.com>
2025-12-11 14:39:04 +0100

    event driven stats microservice

---
+++ StatsService/Program.cs
@@ -0,0 +1,93 @@
+using Common;
+using Contracts;
+using JasperFx.Events;
+using JasperFx.Events.Projections;
+using Marten;
+using StatsService.Models;
+using StatsService.Projections;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+await builder.UserWolverineWithRabbitMqAsync(opts =>
+{
+    opts.ApplicationAssembly = typeof(Program).Assembly;
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("statDb")!);
+
+    opts.Events.StreamIdentity = StreamIdentity.AsString;
+    opts.Events.AddEventType<QuestionCreated>();
+    opts.Events.AddEventType<UserReputationChanged>();
+
+    // Create indexes for performance
+    opts.Schema.For<TagDailyUsage>()
+        .Index(x => x.Tag)
+        .Index(x => x.Date);
+    
+    opts.Schema.For<UserReputationChanged>()
+        .Index(x => x.UserId)
+        .Index(x => x.Occured);
+    
+    
+    // Apply the projection immediately on incoming msg
+    opts.Projections.Add(new TrendingTagsProjection(), ProjectionLifecycle.Inline);
+    opts.Projections.Add(new TopUsersProjection(), ProjectionLifecycle.Inline);
+
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/stats/trending-tags", async (IQuerySession session) =>
+{
+    var today = DateOnly.FromDateTime(DateTime.UtcNow.Date);
+    var start = today.AddDays(-6);
+
+    var rows = await session.Query<TagDailyUsage>()
+        .Where(x => x.Date >= start && x.Date <= today)
+        .Select(x => new { x.Tag, x.Count })
+        .ToListAsync();
+
+    var top = rows
+        .GroupBy(x => x.Tag)
+        .Select(x => new { tag = x.Key, count = x.Sum(t => t.Count) })
+        .OrderByDescending(x => x.count)
+        .Take(5)
+        .ToList();
+
+    return Results.Ok(top);
+});
+
+app.MapGet("/stats/top-users", async (IQuerySession session) =>
+{
+    var today =  DateOnly.FromDateTime(DateTime.UtcNow.Date);
+    var start = today.AddDays(-6);
+
+    var rows = await session.Query<UserDailyReputation>()
+        .Where(x => x.Date >= start && x.Date <= today)
+        .Select(x => new
+        {
+                x.UserId,
+                x.Delta
+        })
+        .ToListAsync();
+    
+    var top = rows.GroupBy(x => x.UserId)
+        .Select(g => new {userId = g.Key, delta = g.Sum(x => x.Delta) })
+        .OrderByDescending(x => x.delta)
+        .Take(5)
+        .ToList();
+    
+    return Results.Ok(top);
+});
+
+
+app.Run();
